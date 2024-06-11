a7d2d31b1f087b1f2faca83bb40e5ca0da0a2a1f
luissmontess <a01737664@tec.mx>
2024-06-10 20:25:30 -0600

    all done

---
+++ taxi_be/lib/taxi_be_web/jobs/taxi_allocation_job.ex
@@ -0,0 +1,148 @@
+defmodule TaxiBeWeb.TaxiAllocationJob do
+  use GenServer
+
+  def start_link(request, name) do
+    GenServer.start_link(__MODULE__, request, name: name)
+  end
+
+  def init(request) do
+    Process.send(self(), :step1, [:nosuspend])
+    {:ok, %{request: request}}
+  end
+
+  def handle_info(:step1, %{request: request}) do
+    Process.send(self(), :block1, [:nosuspend])
+
+    task = Task.async(fn ->
+      compute_ride_fare(request)
+      |> notify_customer_ride_fare()
+    end)
+
+    Task.await(task)
+
+    %{"pickup_address" => pickup} = request
+
+    clientcoord = TaxiBeWeb.Geolocator.geocode(pickup)
+    {_, clientpos} = clientcoord
+    taxis = select_candidate_taxis(request)
+
+    dbtaxis = orderTaxis(clientpos, taxis)
+    IO.inspect(dbtaxis)
+    {:noreply, %{request: request, candidates: dbtaxis, status: NotAccepted}}
+  end
+
+
+  def handle_info(:block1, %{request: request, candidates: taxis, status: NotAccepted} = state) do
+    if taxis != [] do
+      taxi = hd(taxis)
+      %{
+        "pickup_address" => pickup_address,
+        "dropoff_address" => dropoff_address,
+        "booking_id" => booking_id
+      } = request
+
+      TaxiBeWeb.Endpoint.broadcast(
+      "driver:" <> taxi.nickname,
+      "booking_request",
+        %{
+          msg: "Viaje de '#{pickup_address}' a '#{dropoff_address}'",
+          bookingId: booking_id,
+          status: true
+      })
+
+      Process.send_after(self(),:timeout1, 15000)
+      {:noreply, %{request: request, candidates: tl(taxis), contacted_taxi: taxi, status: NotAccepted}}
+    else
+      %{
+        "username" => customer_username
+      } = request
+
+      TaxiBeWeb.Endpoint.broadcast("customer:"<>customer_username, "booking_request", %{msg: "Hubo un problema"})
+      {:noreply, %{state | contacted_taxi: ""}}
+    end
+  end
+
+  def handle_info(:timeout1, %{request: _request, status: NotAccepted} = state) do
+    Process.send(self(), :block1, [:nosuspend])
+    {:noreply, state}
+  end
+
+  def handle_info(:timeout1, %{request: _request, status: Accepted} = state) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:process_accept, driver_username}, %{request: request, status: NotAccepted, contacted_taxi: taxi} = state) do
+
+    IO.inspect(state)
+
+
+    if driver_username == taxi.nickname do
+      %{
+        "username" => customer_username
+      } = request
+
+      TaxiBeWeb.Endpoint.broadcast("customer:"<>customer_username, "booking_request", %{msg: "Tu taxi esta en camino"})
+      # Process.send(self(), :block1, [:nosuspend])
+
+      {:noreply, state |> Map.put(:status, Accepted)}
+    else
+      TaxiBeWeb.Endpoint.broadcast("driver:"<>driver_username, "booking_notification", %{msg: "Muy tarde para aceptar"})
+
+      {:noreply, state}
+    end
+
+
+
+  end
+
+  def handle_cast({:process_accept, driver_username}, %{request: request, status: Accepted} = state) do
+    TaxiBeWeb.Endpoint.broadcast("driver:"<>driver_username, "booking_notification", %{msg: "Ha sido aceptado por otro socio"})
+    # Process.send(self(), :block1, [:nosuspend])
+
+    {:noreply, state}
+
+  end
+
+  def handle_cast({:process_reject, driver_username}, state) do
+    # IO.inspect(state)
+    Process.send(self(), :block1, [:nosuspend])
+    {:noreply, state}
+  end
+
+  def compute_ride_fare(request) do
+    %{
+      "pickup_address" => pickup_address,
+      "dropoff_address" => dropoff_address
+     } = request
+
+    coord1 = TaxiBeWeb.Geolocator.geocode(pickup_address)
+    coord2 = TaxiBeWeb.Geolocator.geocode(dropoff_address)
+    {distance, _duration} = TaxiBeWeb.Geolocator.distance_and_duration(coord1, coord2)
+    {request, Float.ceil(distance/300)}
+  end
+
+  def notify_customer_ride_fare({request, fare}) do
+    %{"username" => customer} = request
+    TaxiBeWeb.Endpoint.broadcast("customer:" <> customer, "booking_request", %{msg: "Ride fare: #{fare}"})
+  end
+
+  def orderTaxis(pickup, taxis) do
+    positions = taxis |> Enum.map(fn taxi -> [taxi.longitude, taxi.latitude] end)
+    taxi_relative_positions = TaxiBeWeb.Geolocator.destination_and_duration(positions, pickup)
+    finished_list =
+      Enum.zip([taxis, taxi_relative_positions])
+      |> Enum.sort()
+      |> IO.inspect()
+      |> Enum.map(fn {item, _} -> item end)
+
+    finished_list
+  end
+
+  def select_candidate_taxis(%{"pickup_address" => _pickup_address}) do
+    [
+      %{nickname: "frodo", latitude: 19.0319783, longitude: -98.2349368}, # Angelopolis
+      %{nickname: "samwise", latitude: 19.0061167, longitude: -98.2697737}, # Arcangeles
+      %{nickname: "merry", latitude: 19.0092933, longitude: -98.2473716} # Paseo Destino
+    ]
+  end
+end
